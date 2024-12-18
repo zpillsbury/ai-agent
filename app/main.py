8bf6d01fa233e6489b250b74ccdeb4a98b7a4985
Zpillsbury <zpillbury98@gmail.com>
2024-12-18 09:47:13 -0500

    async lang graph set up

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
+from typing import Annotated, TypedDict
+
+import anyio
+from langchain_core.messages import BaseMessage
 from langchain_openai import ChatOpenAI
+from langgraph.graph import END, START, StateGraph
+from langgraph.graph.message import add_messages
 
 from .utilities.settings import settings
 
@@ -7,5 +13,41 @@ llm = ChatOpenAI(
     model_name="gpt-4o-mini",
     max_retries=2,
 )
-r = llm.invoke("How are you?")
-print(r.content)
+
+
+class State(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+
+graph_builder = StateGraph(State)
+
+
+async def chatbot(state: State) -> State:
+    """
+    Chatbot
+    """
+    response_message = await llm.ainvoke(state["messages"])
+
+    return {"messages": [response_message]}
+
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+
+async def main() -> None:
+    """
+    AI Agent
+    """
+    async for event in graph.astream({"messages": [("user", "how are you?")]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+    return None
+
+
+if __name__ == "__main__":
+    anyio.run(main)
