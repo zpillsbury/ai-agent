5eefb569c2a8d100200591ab109b6831bec1b772
Zpillsbury <zpillbury98@gmail.com>
2024-12-19 11:22:19 -0500

    add mongo client, added config, consolidate nodes and edges into one function

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 from datetime import datetime, timezone
-from typing import Annotated, TypedDict
+from typing import Annotated, Any, TypedDict
 
 import anyio
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
+from langchain_core.runnables import RunnableConfig
 from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.mongodb.aio import AsyncMongoDBSaver
 from langgraph.graph import START, StateGraph
 from langgraph.graph.message import add_messages
+from langgraph.graph.state import CompiledStateGraph
 from langgraph.prebuilt import ToolNode, tools_condition
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from .utilities.settings import settings
+
+async_mongodb_client: AsyncIOMotorClient[Any] = AsyncIOMotorClient(
+    settings.mongo_uri.get_secret_value()
+)
 
 now = datetime.now(timezone.utc)
 
@@ -21,21 +29,16 @@ Current Date: {now}
 
 web_search = TavilySearchResults(max_results=2)
 tools = [web_search]
-
 llm = ChatOpenAI(
     openai_api_key=settings.openai_key,
     model_name="gpt-4o-mini",
     max_retries=2,
 )
-
 llm_with_tools = llm.bind_tools(tools)
 
 
 class State(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
-
-
-graph_builder = StateGraph(State)
 
 
 async def chatbot(state: State) -> State:
@@ -47,33 +50,51 @@ async def chatbot(state: State) -> State:
     return {"messages": [response_message]}
 
 
-graph_builder.add_node("chatbot", chatbot)
+async def get_graph() -> CompiledStateGraph:
+    """
+    Get the graph
+    """
+    checkpointer = AsyncMongoDBSaver(
+        client=async_mongodb_client,
+        db_name="ai",
+        checkpoint_collection_name="checkpoints",
+        writes_collection_name="checkpoint_writes",
+    )
 
-tool_node = ToolNode(tools=tools)
-graph_builder.add_node("tools", tool_node)
+    graph_builder = StateGraph(State)
 
-graph_builder.add_edge(START, "chatbot")
+    graph_builder.add_node("chatbot", chatbot)
 
-graph_builder.add_conditional_edges("chatbot", tools_condition)
-graph_builder.add_edge("tools", "chatbot")
+    tool_node = ToolNode(tools=tools)
+    graph_builder.add_node("tools", tool_node)
 
-graph = graph_builder.compile()
+    graph_builder.add_edge(START, "chatbot")
+
+    graph_builder.add_conditional_edges("chatbot", tools_condition)
+    graph_builder.add_edge("tools", "chatbot")
+
+    graph = graph_builder.compile(checkpointer=checkpointer)
+
+    return graph
 
 
-async def run_graph(question: str) -> None:
+async def run_graph(config: RunnableConfig, question: str) -> None:
     """
     Run the graph
     """
+    graph = await get_graph()
+
     async for event in graph.astream(
         {
             "messages": [
                 SystemMessage(content=system_prompt),
                 HumanMessage(content=question),
             ]
-        }
+        },
+        config=config,
+        stream_mode="values",
     ):
-        for value in event.values():
-            print(value["messages"][-1].content)
+        event["messages"][-1].pretty_print()
 
     return None
 
@@ -82,13 +103,15 @@ async def main() -> None:
     """
     AI Agent
     """
+    config = RunnableConfig(configurable={"thread_id": 2})
+
     while True:
         question = input("q: ")
         if question.lower() in ["quit", "exit", "q"]:
             print("Goodbye!")
             break
 
-        await run_graph(question)
+        await run_graph(config=config, question=question)
 
     return None
 
